11608df0f88ab91d8b3b208a386949808a513026
AlexPCoder <alexeypodluzhniy@gmail.com>
2024-08-22 13:21:53 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,66 @@
-package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class WebDriverActions {
+
+    private WebDriver driver;
+
+    // Constructor to initialize the WebDriver
+    public WebDriverActions() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        this.driver = new ChromeDriver();
+    }
+
+    // Method to open a webpage
+    public void openWebPage(String url) {
+        driver.get(url);
+    }
+
+    // Method to find a button using XPath and click on it
+    public void clickButtonByXPath(String buttonXPath) {
+        WebElement button = driver.findElement(By.xpath(buttonXPath));
+        button.click();
+    }
+
+    // Method to read the text of an element using XPath
+    public String readMessageByXPath(String messageXPath) {
+        WebElement messageElement = driver.findElement(By.xpath(messageXPath));
+        return messageElement.getText();
+    }
+
+    public void enterTextByXPath(String textFieldXPath, String text) {
+        WebElement textField = driver.findElement(By.xpath(textFieldXPath));
+        textField.clear(); // Clears any existing text in the field
+        textField.sendKeys(text); // Enters the new text
+    }
+
+    // Method to check if the email value in the element matches the expected value
+    public boolean checkEmailValue(String xpathNewElement, String expectedEmail) {
+        WebElement emailElement = driver.findElement(By.xpath(xpath));
+        String actualEmail = emailElement.getText();
+        return actualEmail.equals(expectedEmail);
+    }
+
+    // Method to close the browser
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    // Main method to demonstrate the use of the methods
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        WebDriverActions actions = new WebDriverActions();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Example usage
+        String url = "http://example.com";
+        String buttonXPath = "//*[@id='button-id']"; // Replace with the actual XPath
+        String messageXPath = "//*[@id='message-id']"; // Replace with the actual XPath
+
+        actions.openWebPage(url);
+        actions.clickButtonByXPath(buttonXPath);
+        String message = actions.readMessageByXPath(messageXPath);
+        System.out.println("Message: " + message);
+        actions.closeBrowser();
     }
 }
--- src/test/java/SeleniumWebDriverTest.java
+++ src/test/java/SeleniumWebDriverTest.java
@@ -1,2 +1,86 @@
-public class SeleniumWebDriverTest {
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class WebDriverActionsTest {
+
+    private WebDriverActions actions;
+
+    @BeforeMethod
+    public void setUp() {
+        actions = new WebDriverActions();
+    }
+
+    @Test
+    public void testButtonClickAndReadMessage() {
+        // URL to open
+        String url = "https://demoqa.com/buttons";
+        // XPath for the button
+        String buttonXPath1 = "//*[@id=\"item-4\"]"; // Replace with the actual XPath
+        String buttonXPath2 = "//*[@id=\"DdfqK\"]";
+        // XPath for the message element
+        String messageXPath = "//*[@id=\"dynamicClickMessage\"]"; // Replace with the actual XPath
+
+        // Perform actions
+        actions.openWebPage(url);
+        actions.clickButtonByXPath(buttonXPath1);
+        actions.clickButtonByXPath(buttonXPath2);
+        String message = actions.readMessageByXPath(messageXPath);
+
+        // Assert or print the message (use assertions in real tests)
+        System.out.println("Message: " + message);
+    }
+
+    @Test
+    public void testFillAndEditForm() {
+        // URL to open
+        String url = "https://demoqa.com/webtables";
+        // XPath for the button
+        String buttonAddXPath = "//*[@id=\"addNewRecordButton\"]";
+        String buttonSubmitXPath = "//*[@id=\"submit\"]";
+        String buttonEditXPath = "//*[@id=\"edit-record-4\"]";
+        // XPath
+        String textFieldFNameXPath = "//*[@id=\"firstName\"]";
+        String textFieldSNameXPath = "//*[@id=\"lastName\"]";
+        String textEmailXPath = "//*[@id=\"userEmail\"]";
+        String textFieldAgeXPath = "//*[@id=\"age\"]";
+        String textFieldSalaryXPath = "//*[@id=\"salary\"]";
+        String textFieldDepartmentXPath = "//*[@id=\"department\"]";
+        String xpathNewElement = "//div[@class='rt-td' and @role='gridcell' and text()='p@email.com']\n";
+        String xpathUpdatedElement = "//div[@class='rt-td' and @role='gridcell' and text()='new@email.com']\n";
+        // Text to enter into the text field
+        String textToEnterFName = "Alex";
+        String textToEnterSName = "Pod";
+        String textToEnterEmail = "Alex@email.com";
+        String textToEnterAge = "1";
+        String textToEnterSalary = "2";
+        String textToEnterDepartment = "IT";
+        String expectedEmail = "p@email.com";
+
+        // Perform actions
+        actions.openWebPage(url);
+        // Create new record
+        actions.clickButtonByXPath(buttonAddXPath);
+        actions.enterTextByXPath(textFieldFNameXPath, textToEnterFName);
+        actions.enterTextByXPath(textFieldSNameXPath, textToEnterSName);
+        actions.enterTextByXPath(textEmailXPath, textToEnterEmail);
+        actions.enterTextByXPath(textFieldAgeXPath, textToEnterAge);
+        actions.enterTextByXPath(textFieldSalaryXPath, textToEnterSalary);
+        actions.enterTextByXPath(textFieldDepartmentXPath, textToEnterDepartment);
+        actions.clickButtonByXPath(buttonSubmitXPath);
+        // Check that new record exist
+        actions.checkEmailValue(xpathNewElement, expectedEmail);
+        // Check edit new record
+        actions.clickButtonByXPath(buttonEditXPath);
+        actions.enterTextByXPath(textEmailXPath, "new@email.com");
+        actions.clickButtonByXPath(buttonSubmitXPath);
+        // Check that new record exist
+        actions.checkEmailValue(xpathUpdatedElement, "new@email.com");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Close the browser after the test
+        actions.closeBrowser();
+    }
 }
